bb5e5b6cd6dec2923be153384873d0e368ecda7f
Nadya Povalyaeva <iriejamaica@outlook.com>
2019-05-12 13:55:36 +0300

    Add Specialist

---
+++ Specialist/Data/Hash.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Specialist.Data
+{
+    public static class Hash
+    {
+        public static string FindHash(string input)
+        {
+            byte[] tmpSource = ASCIIEncoding.ASCII.GetBytes(input);
+            byte[] tmpHash = new MD5CryptoServiceProvider().ComputeHash(tmpSource);
+            return ByteArrayToString(tmpHash);
+        }
+
+        private static string ByteArrayToString(byte[] arrInput)
+        {
+            int i;
+            StringBuilder sOutput = new StringBuilder(arrInput.Length);
+            for (i = 0; i < arrInput.Length; i++)
+            {
+                sOutput.Append(arrInput[i].ToString("X2"));
+            }
+            return sOutput.ToString();
+        }
+    }
+}
---
+++ Specialist/Data/SpecialistContext.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Specialist.Data
+{
+    public class SpecialistContext
+    {
+        public string ConnectionString { get; set; }
+
+        public SpecialistContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+    }
+}
