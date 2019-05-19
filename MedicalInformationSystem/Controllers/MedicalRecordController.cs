f1ca6ac5daf471000fdb1095db078e0af7510c55
Nadya Povalyaeva <iriejamaica@outlook.com>
2019-05-19 16:16:17 +0300

    Add "disease" dependence and resolve the merge conflict

--- MedicalInformationSystem/Controllers/MedicalRecordController.cs
+++ MedicalInformationSystem/Controllers/MedicalRecordController.cs
@@ -16,19 +16,17 @@ namespace MedicalInformationSystem.Controllers
         [HttpGet()]
         public IEnumerable<Record> GetAllMedicalRecords()
         {
-<<<<<<< HEAD
             var list = new List<Record>();
             list.Add(new Record() { RecordId = 123, FirstName = "Настена", LastName = "Дайгод", PatientId = 1, ICD10 = "Е50.0", Year = 2016, ChargeDate = "01.03.2019" });
             list.Add(new Record() { RecordId = 222, FirstName = "Надюша", LastName = "Поваляева", PatientId = 2, ICD10 = "Е50.1", Year = 2008, ChargeDate = "03.03.2019" });
             return list;
             /* MedicalRecordContext context = HttpContext.RequestServices.GetService(typeof(MedicalRecordContext)) as MedicalRecordContext;
              MedicalRecord.Handlers.GetAllMedicalRecordsHandler handler = new MedicalRecord.Handlers.GetAllMedicalRecordsHandler(context);
-             return handler.Handle(); */
-=======
+             return handler.Handle(); 
+
             MedicalRecordContext context = HttpContext.RequestServices.GetService(typeof(MedicalRecordContext)) as MedicalRecordContext;
             GetAllMedicalRecordsHandler handler = new GetAllMedicalRecordsHandler(context);
-            return handler.Handle();
->>>>>>> 195e0be2736d9d7566dbdd6ab8fc0ef8a3ecc6f1
+            return handler.Handle(); */
         }
 
         [HttpGet("[action]")]
